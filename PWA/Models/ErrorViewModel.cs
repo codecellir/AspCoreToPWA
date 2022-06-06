bdb8c2caedde3ba4fc67a70f2f222d3da9cae645
Sohrab Gheisari <sohrabgheisari@hotmail.com>
2022-06-06 23:56:05 +0430

    init

---
+++ PWA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PWA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
